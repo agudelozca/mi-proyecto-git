e0ae4562755837adf316fec27e0fe891eed443e5
agudelozca <cristian.agudelozapata@mercadolibre.com.co>
2024-01-04 16:48:05 -0500

    feat: Examples from class number 7 (panics recover defer) added

--- GoBases/Clase_7/Example_3/main.go
+++ GoBases/Clase_7/Example_3/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
 
+	path := "./Clase_7/Example_3/customer.txt"
+
+	text := ReadFile(path)
+
+	fmt.Println(text)
+	fmt.Println("Ejecucion finalizada")
+
+}
+
+func ReadFile(path string) string {
+	file, err := os.Open(path)
+
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	if err != nil {
+		panic("The indicated file was not found or is damaged")
+	}
+
+	// read bytes from file
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		panic("An error occurred while reading the file")
+	}
+
+	defer file.Close()
+	return string(bytes)
 }
